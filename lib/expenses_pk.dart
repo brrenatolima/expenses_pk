3ce5f742db8832588c879a6c0067a4069788afab
Renato Lima <renatomdd@gmail.com>
2023-06-27 07:31:09 -0300

    first commit

---
+++ lib/expenses_pk.dart
@@ -0,0 +1,29 @@
+library expenses_pk;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  double currentBalance(List<Transaction> lista) => lista.fold(0.0, (previousValue, element) => previousValue + element.amount);
+}
+
+
+class Transaction {
+  String id;
+  String title;
+  double amount;
+  DateTime date;
+  String? location;
+  String? imageUrl;
+
+    Transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+    this.location,
+    this.imageUrl
+  });
+
+}
