244d02384b4e2523a377a8bcd847800ddf4dafbf
Renato Lima <renatomdd@gmail.com>
2023-06-28 20:41:58 -0300

    melhoria nos testes

--- test/expenses_pk_test.dart
+++ test/expenses_pk_test.dart
@@ -1,12 +1,17 @@
+import 'package:expenses_pk/expenses_pk.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:expenses_pk/expenses_pk.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('expenses pk ...', (tester) async {
+    
+    List<Tr> lista = [
+      Tr(id: "123", title: "Teste01", amount: 100, date: DateTime.now()),
+      Tr(id: "123", title: "Teste01", amount: 50, date: DateTime.now()),
+    ];
+
+    Calculator calc = Calculator();
+    expect(calc.currentBalance(lista), 150);
+
+
   });
 }
